865126fa7b4ba58a5f83cf8172c215118bb38494
LouzirMohamedAziz <medaziz.louzir@gmail.com>
2023-05-24 00:55:36 +0100

    Resolving CRUD issues

--- src/main/java/com/zay/fithub/Controllers/UserController.java
+++ src/main/java/com/zay/fithub/Controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.zay.fithub.Controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,33 +11,84 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zay.fithub.Entities.User;
-import com.zay.fithub.Repositories.UserRepository;
+import com.zay.fithub.Services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
-    private  UserRepository userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-    @PostMapping("/addUser")
-    public String createUser(@RequestBody User user) {
-        userRepository.save(user);
-        return  "Added user:"+user.getFirstName()+" "+user.getName()+"with id: "+user.getId();
+    @PostMapping
+    public User createProduct(@RequestBody User user) {
+        return userService.createUser(user);
     }
 
     @GetMapping
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public List<User> getAllProducts() {
+        return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public Optional<User> getUser(@PathVariable int id){
-        return userRepository.findById((long) id);
+    public User getProductById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable int id){
-        userRepository.deleteById((long) id);
-        return "user deleted with id: "+id;
+    public void deleteProduct(@PathVariable Long id) {
+        userService.deleteUser(id);
     }
 }
+
+
+
+
+
+
+// package com.zay.fithub.Controllers;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.zay.fithub.Entities.User;
+// import com.zay.fithub.Repositories.UserRepository;
+
+// @RestController
+// @RequestMapping("/users")
+// public class UserController {
+
+//     private  UserRepository userRepository;
+
+//     @PostMapping("/addUser")
+//     public String createUser(@RequestBody User user) {
+//         userRepository.save(user);
+//         return  "Added user:"+user.getFirstName()+" "+user.getName()+"with id: "+user.getId();
+//     }
+
+//     @GetMapping
+//     public List<User> getUsers() {
+//         return userRepository.findAll();
+//     }
+
+//     @GetMapping("/{id}")
+//     public Optional<User> getUser(@PathVariable int id){
+//         return userRepository.findById((long) id);
+//     }
+
+//     @DeleteMapping("/{id}")
+//     public String deleteUser(@PathVariable int id){
+//         userRepository.deleteById((long) id);
+//         return "user deleted with id: "+id;
+//     }
+// }
---
+++ src/main/java/com/zay/fithub/Services/UserService.java
@@ -0,0 +1,36 @@
+package com.zay.fithub.Services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.zay.fithub.Entities.User;
+import com.zay.fithub.Repositories.UserRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Cart not found with id: " + id));
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
