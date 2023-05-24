f34f7c990f80775773645500c98406062324a6ab
LouzirMohamedAziz <medaziz.louzir@gmail.com>
2023-05-24 16:08:45 +0100

    Resolving CRUD operations

--- src/main/java/com/zay/fithub/FithubApplication.java
+++ src/main/java/com/zay/fithub/FithubApplication.java
@@ -3,6 +3,7 @@ package com.zay.fithub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -10,7 +11,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan(basePackages = "com.zay.fithub")
 @EnableMongoRepositories(basePackages = "com.zay.fithub.Repositories")
 @EntityScan("com.zay.fithub.Entities")
-@SpringBootApplication()
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class FithubApplication {
 
 	public static void main(String[] args) {
